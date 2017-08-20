8e0afc06d221d86677a172fdb7d1388225504ba6
Darius Morawiec <darius.morawiec@nok.onl>
2017-08-20 16:45:56 +0200

    Add specific required-property to all arguments

--- resp/__main__.py
+++ resp/__main__.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
 
 from Parser import Parser
 
 
-def main(argv):
+def main():
 
     # Arguments:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--redis_cmd', type=str, default='')
-    parser.add_argument('-i', '--input', type=str, default='')
-    parser.add_argument('-d', '--delimiter', type=str, default=',')
-    parser.add_argument('-p', '--pipe', action='store_true')
+    parser.add_argument('-r', '--redis_cmd', type=str, default='', required=True)
+    parser.add_argument('-i', '--input', type=str, default='', required=False)
+    parser.add_argument('-d', '--delimiter', type=str, default=',', required=False)
+    parser.add_argument('-p', '--pipe', action='store_true', required=False)
     args = parser.parse_args()
 
     # Parser:
     Parser(args.input, args.redis_cmd, args.delimiter, args.pipe)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
